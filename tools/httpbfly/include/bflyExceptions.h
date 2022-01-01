65a7d88cf737ccd581bda15ebdbfe9212f9e8445
Bierscheider Christian <christian.bierschneider@web.de>
2022-01-01 14:00:54 +0100

    Added first files for httpbfly tool

---
+++ tools/httpbfly/include/bflyExceptions.h
@@ -0,0 +1,155 @@
+
+#ifndef HTTPBFLY_BFLYEXCEPTIONS_H
+#define HTTPBFLY_BFLYEXCEPTIONS_H
+
+#include <exception>
+#include <iostream>
+
+namespace tools
+{
+
+/**
+ * Class ButterflyException
+ */
+class ButterflyException : public std::exception
+{
+
+public:
+    std::string _error;
+
+    explicit ButterflyException(const std::string &error) : _error(error) {}
+
+    const char * what() const noexcept override
+    {
+        return _error.c_str();
+    }
+
+    virtual std::string getType() const
+    {
+        return "ButterflyException";
+    }
+};
+
+/**
+ * Class EncryptionException
+ */
+class EncryptorException : public ButterflyException
+{
+
+public:
+    explicit EncryptorException(const std::string &error) : ButterflyException(error) {}
+
+    std::string getType() const override
+    {
+        return "EncryptorException";
+    }
+};
+
+/**
+ * Class DecryptorException
+ */
+class DecryptorException : public ButterflyException
+{
+
+public:
+    explicit DecryptorException(const std::string &error) : ButterflyException(error) {}
+
+    std::string getType() const override
+    {
+        return "DecryptorException";
+    }
+};
+
+/**
+ * Class RSAEncryptionException
+ */
+class RSAEncryptionException : public EncryptorException
+{
+
+public:
+    explicit RSAEncryptionException(const std::string &error) : EncryptorException(error) {}
+
+    std::string getType() const override
+    {
+        return "RSAEncryptionException";
+    }
+};
+
+/**
+ * Class RSADecryptionException
+ */
+class RSADecryptionException : public DecryptorException
+{
+
+public:
+    explicit RSADecryptionException(const std::string &error) : DecryptorException(error) {}
+
+    std::string getType() const override
+    {
+        return "RSADecryptionException";
+    }
+};
+
+/**
+ * Class AESEncryptionException
+ */
+class AESEncryptionException : public EncryptorException
+{
+
+public:
+    explicit AESEncryptionException(const std::string &error) : EncryptorException(error) {}
+
+    std::string getType() const override
+    {
+        return "AESEncryptionException";
+    }
+};
+
+/**
+ * Class AESDecryptionException
+ */
+class AESDecryptionException : public DecryptorException
+{
+
+public:
+    explicit AESDecryptionException(const std::string &error) : DecryptorException(error) {}
+
+    std::string getType() const override
+    {
+        return "AESDecryptionException";
+    }
+};
+
+/**
+ * Class SQLDatabaseException
+ */
+class SQLDatabaseException : public ButterflyException
+{
+
+public:
+    explicit SQLDatabaseException(const std::string &error) : ButterflyException(error) {}
+
+    std::string getType() const override
+    {
+        return "SQLDatabaseException";
+    }
+};
+
+/**
+ * Class SocketException
+ */
+class SocketException : public ButterflyException
+{
+
+public:
+    explicit SocketException(const std::string &error) : ButterflyException(error) {}
+
+    std::string getType() const override
+    {
+        return "SocketException";
+    }
+};
+
+} // namespace tools
+
+#endif
---
+++ tools/httpbfly/include/httpResponse.h
@@ -0,0 +1,93 @@
+
+#ifndef HTTPBFLY_HTTPRESPONSE_H
+#define HTTPBFLY_HTTPRESPONSE_H
+
+#include "httpMSGSchema.h"
+
+namespace tools
+{
+
+/**
+ * Class HTTPResponse for outgoing HTTP Responses (Server -> Client)
+ */
+class HTTPResponse: public HTTPMSGSchema
+{
+
+private:
+    std::string _reasonPhrase;
+    size_t _statusCode;
+
+public:
+
+    /**
+     * Constructor HTTPResponse
+     *
+     * Usage:
+     *      std::unique_ptr<butterfly::HTTPResponse> _httpResponse(new HTTPResponse());
+     *
+     *      _httpResponse->setProtocol(HTTP1_1);
+     *      _httpResponse->setReasonPhrase(302);
+     *      _httpResponse->setHTTPHeader("Content-Type", "text/html; charset=utf8");
+     *      _httpResponse->addBody("<!DOCTYPE html><html><body><h1>Test Message</h1></body></html>");
+     *      _httpResponse->setHTTPHeader("Content-Length", std::to_string(_httpResponse->getBody().length()));
+     *
+     *      _httpResponse->prepareOutgoing();
+     *
+     */
+    HTTPResponse();
+
+    /**
+     * Destructor HTTPResponse
+     */
+    ~HTTPResponse() override = default;
+
+    /**
+     * Setter for the status code
+     *
+     * @param statusCode: status code
+     */
+    void setStatusCode(size_t statusCode);
+
+    /**
+     * Getter for the status code
+     *
+     * @return status code
+     */
+    inline size_t getStatusCode() const { return _statusCode; }
+
+    /**
+     * Setter for the reason phrase
+     *
+     * @param statusCode: status code
+     */
+    void setReasonPhrase(size_t statusCode);
+
+    /**
+     * Getter for the reason phrase
+     *
+     * @return reason phrase as std::string
+     */
+    inline std::string getReasonPhrase() const { return _reasonPhrase; }
+
+    /**
+     * Getter for the response size
+     *
+     * @return size of the response
+     */
+    inline size_t getResponseSize() const { return _httpData.length(); }
+
+    /**
+     * Method to parse the incoming HTTP Responses
+     */
+    void parseIncoming() override;
+
+    /**
+     * Method to prepare the HTTP Responses for further usage
+     */
+    void prepareOutgoing() override;
+
+};
+
+} // namespace tools
+
+#endif
