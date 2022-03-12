9406a7c4fbc461fe5cf1e9d57e231720ef87c33c
Bierscheider Christian <christian.bierschneider@web.de>
2022-03-12 17:30:48 +0100

    Added new directoryIteratorTest class

--- test/directoryIteratorTest.cpp
+++ test/directoryIteratorTest.cpp
@@ -10,6 +10,8 @@ class DirectoryIteratorTest : public ::testing::Test
 {
 
 protected:
+    std::string testDirectory = "../test/";
+    std::string testFile = "../test/butterflyUnitTests.cpp";
     std::unique_ptr<butterfly::DirectoryIterator> directoryIterator;
 
     void SetUp() override
@@ -22,3 +24,99 @@ protected:
 
     }
 };
+
+/**
+ * Testcase for testing the isDirectory
+ */
+TEST_F(DirectoryIteratorTest, isDirectory)
+{
+
+    bool isDir = directoryIterator->isDirectory(testDirectory);
+
+    EXPECT_TRUE(isDir == true);
+}
+
+/**
+ * Testcase for testing the isFile
+ */
+TEST_F(DirectoryIteratorTest, isFile)
+{
+
+    bool isFile = directoryIterator->isFile(testFile);
+
+    EXPECT_TRUE(isFile == true);
+}
+
+/**
+ * Testcase for testing the getAllFiles
+ */
+TEST_F(DirectoryIteratorTest, getAllFiles)
+{
+
+    auto files = directoryIterator->getAllFiles(testDirectory);
+
+    EXPECT_TRUE(files.size() > 0);
+
+    for(auto &file: files)
+    {
+        auto isFile = directoryIterator->isFile(file);
+        EXPECT_TRUE(isFile == true);
+    }
+}
+
+/**
+ * Testcase for testing the getAllDirectories
+ */
+TEST_F(DirectoryIteratorTest, getAllDirectories)
+{
+
+    auto files = directoryIterator->getAllDirectories(testDirectory);
+
+    EXPECT_TRUE(files.size() > 0);
+
+    for(auto &file: files)
+    {
+        auto isDir = directoryIterator->isDirectory(file);
+        EXPECT_TRUE(isDir == true);
+    }
+}
+
+/**
+ * Testcase for testing the getFilesFromDir
+ */
+TEST_F(DirectoryIteratorTest, getFilesFromDir)
+{
+
+    auto files = directoryIterator->getFilesFromDir(testDirectory);
+
+    EXPECT_TRUE(files.size() > 0);
+
+    for(auto &file: files)
+    {
+        auto isFile = directoryIterator->isFile(file);
+        EXPECT_TRUE(isFile == true);
+    }
+}
+
+/**
+ * Testcase for testing the getFileSize
+ */
+TEST_F(DirectoryIteratorTest, getFileSize)
+{
+
+    uintmax_t fileSize = directoryIterator->getFileSize(testFile);
+
+    EXPECT_TRUE(fileSize > 0);
+}
+
+/**
+ * Testcase for testing the getFileExtension
+ */
+TEST_F(DirectoryIteratorTest, getFileExtension)
+{
+
+    std::string fileExt = directoryIterator->getFileExtension(testFile);
+
+    EXPECT_TRUE(fileExt == ".cpp");
+
+}
