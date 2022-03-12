f7e1d0c58ae6ccb97bd143c4b29b837373acab02
Bierscheider Christian <christian.bierschneider@web.de>
2022-03-12 18:14:07 +0100

    Added httpRequest and httpResponse Testclasses

--- test/httpResponseTest.cpp
+++ test/httpResponseTest.cpp
@@ -4,7 +4,7 @@
 #include "httpResponse.h"
 
 /**
- * Testclass HTTPResponse
+ * Testclass HTTPResponseTest
  */
 class HTTPResponseTest : public ::testing::Test
 {
@@ -17,7 +17,9 @@ protected:
         httpResponse = std::unique_ptr<butterfly::HTTPResponse>(new butterfly::HTTPResponse());
         // Set http response data
         httpResponse->setProtocol(butterfly::Protocol::HTTP1_1);
+        httpResponse->setStatusCode(200);
         httpResponse->setReasonPhrase(200);
+        httpResponse->setHTTPHeader("User-Agent", "butterfly");
         httpResponse->setHTTPHeader("Content-Type", "text/html; charset=utf8");
         httpResponse->addBody("<!DOCTYPE html><html><body><h1>Test Message</h1></body></html>");
         httpResponse->setHTTPHeader("Content-Length", std::to_string(httpResponse->getBody().length()));
@@ -32,23 +34,74 @@ protected:
 };
 
 /**
- * Testcase for testing the setgetStatusCode
+ * Testcase for testing the getProtocol
  */
-TEST_F(HTTPResponseTest, setgetStatusCode)
+TEST_F(HTTPResponseTest, getProtocol)
 {
-    httpResponse->setStatusCode(500);
-    size_t statuscode = httpResponse->getStatusCode();
 
-    EXPECT_TRUE(statuscode == 500);
+    butterfly::Protocol prot = httpResponse->getProtocol();
+
+    EXPECT_TRUE(prot == butterfly::Protocol::HTTP1_1);
 }
 
 /**
- * Testcase for testing the setgetReasonPhrase
+ * Testcase for testing the getStatusCode
  */
-TEST_F(HTTPResponseTest, setgetReasonPhrase)
+TEST_F(HTTPResponseTest, getStatusCode)
 {
-    httpResponse->setReasonPhrase(500);
+    size_t statuscode = httpResponse->getStatusCode();
+
+    EXPECT_TRUE(statuscode == 200);
+}
+
+/**
+ * Testcase for testing the getReasonPhrase
+ */
+TEST_F(HTTPResponseTest, getReasonPhrase)
+{
     std::string reason = httpResponse->getReasonPhrase();
 
-    EXPECT_TRUE(reason == "Internal Server Error");
+    EXPECT_TRUE(reason == "OK");
+}
+
+/**
+ * Testcase for testing the getHTTPHeader
+ */
+TEST_F(HTTPResponseTest, getHTTPHeader)
+{
+    std::string httpHeaderUserAgent = httpResponse->getHTTPHeader("User-Agent");
+    std::string httpHeaderContent = httpResponse->getHTTPHeader("Content-Type");
+
+    EXPECT_TRUE(httpHeaderUserAgent == "butterfly");
+    EXPECT_TRUE(httpHeaderContent == "text/html; charset=utf8");
+}
+
+/**
+ * Testcase for testing the getBody
+ */
+TEST_F(HTTPResponseTest, getBody)
+{
+    std::string body = httpResponse->getBody();
+
+    EXPECT_TRUE(body == "<!DOCTYPE html><html><body><h1>Test Message</h1></body></html>");
+}
+
+/**
+ * Testcase for testing the getHTTPData
+ */
+TEST_F(HTTPResponseTest, getHTTPData)
+{
+    std::string httpdata = httpResponse->getHTTPData();
+
+    EXPECT_TRUE(!httpdata.empty());
+}
+
+/**
+ * Testcase for testing the getRequestSize
+ */
+TEST_F(HTTPResponseTest, getRequestSize)
+{
+    size_t httpDatasize = httpResponse->getResponseSize();
+
+    EXPECT_TRUE( httpDatasize > 0);
 }
