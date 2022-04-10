4078fb0153dcae64bd47a149e52dc9b4b402da1a
Bierscheider Christian <christian.bierschneider@web.de>
2022-04-10 13:59:56 +0200

    Finished the first version of httpbfly tool

--- tools/httpbfly/README.md
+++ tools/httpbfly/README.md
@@ -6,3 +6,15 @@ cmake ../
 make
 ./httpbfly --help
 </code></pre>
+
+Start the `httpblfy` app as server
+<pre><code>
+./httpbfly --server --port 8080
+</code></pre>
+
+Start the `httpbfly` app as client
+<pre><code>
+./httpbfly --client --get --url 127.0.0.1 --port 8080
+./httpbfly --client --post --url http://127.0.0.1:5000/decryption/  --port 5000
+./httpbfly --client --get --url www.google.de --port 80
+</code></pre>
--- tools/httpbfly/src/argumentParser.cpp
+++ tools/httpbfly/src/argumentParser.cpp
@@ -21,7 +21,7 @@ ArgumentParser::Arguments ArgumentParser::parseArgs()
 
     if (_argc > 1)
     {
-        bool found = false, server = false, client = false, ip = false, port = false;
+        bool found = false, server = false, client = false, url = false, port = false, get = false, post = false;
         for (int i = 1; i < _argc; i++)
         {
 
@@ -46,18 +46,18 @@ ArgumentParser::Arguments ArgumentParser::parseArgs()
                 server = true;
                 args.server = true;
 
-            } else if (arg == "-i" || arg == "--ip")
+            } else if (arg == "-u" || arg == "--url")
             {
                 // check end of argc
                 if (i + 1 < _argc && (!strchr(_argv[i + 1], '-')))
                 {
                     found = true;
-                    ip = true;
-                    args.ip = _argv[i + 1];
+                    url = true;
+                    args.url = _argv[i + 1];
 
                 } else
                 {
-                    std::cout << "--ip option requires one argument!" << std::endl;
+                    std::cout << "--url option requires one argument!" << std::endl;
                     exit(1);
                 }
 
@@ -75,6 +75,20 @@ ArgumentParser::Arguments ArgumentParser::parseArgs()
                     std::cout << "--port option requires one argument!" << std::endl;
                     exit(1);
                 }
+
+            } else if (arg == "-g" || arg == "--get")
+            {
+
+                found = true;
+                get = true;
+                args.get = true;
+
+            } else if (arg == "-po" || arg == "--post")
+            {
+
+                found = true;
+                post = true;
+                args.post = true;
 
             } else
             {
@@ -98,9 +112,13 @@ ArgumentParser::Arguments ArgumentParser::parseArgs()
         {
             std::cout << "--server or --client option is mandatory!" << std::endl;
             exit(1);
-        } else if ( client and (!ip) )
+        } else if ( client and (!url) )
         {
-            std::cout << "--client option needs --ip argument!" << std::endl;
+            std::cout << "--client option needs --url argument!" << std::endl;
+            exit(1);
+        } else if (get and post)
+        {
+            std::cout << "Use either --get or --post option" << std::endl;
             exit(1);
         }
     } else
@@ -115,14 +133,18 @@ ArgumentParser::Arguments ArgumentParser::parseArgs()
 void ArgumentParser::showUsage() const
 {
     std::cout << "Usage: \n\t"
-                 + std::string(_projectName) + " --client --port 8080 \n\t"
+                 + std::string(_projectName) + " --get --client --url www.google.de --port 80 \n\t"
+                 + std::string(_projectName) + " --post --client --url http://127.0.0.1:5000/decryption/ --port 5000 \n\t"
                  + std::string(_projectName) + " --server --port 8080 \n\n"
 
                  + "Options:\n"
-                 + "\t-c, --client\t    Creates a client socket\n"
-                 + "\t-s, --server\t    Creates a server socket\n"
-                 + "\t-p,   --port\t    Provide the port\n"
-                 + "\t-h,   --help\t    Show this message and quit"
+                 + "\t-c,  --client\t    Creates an HTTP Client\n"
+                 + "\t-s,  --server\t    Creates an HTTP Server\n"
+                 + "\t-u,  --url\t        Provide the URL\n"
+                 + "\t-p,  --port\t        Provide the port\n"
+                 + "\t-g,  --get\t        HTTP GET Request\n"
+                 + "\t-po, --post\t        HTTP POST Request\n"
+                 + "\t-h,  --help\t        Show this message and quit"
 
                  + "\n\nbutterfly homepage at: https://github.com/bierschi/butterfly"
               << std::endl;
