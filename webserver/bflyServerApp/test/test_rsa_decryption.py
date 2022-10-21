7d162550333886508896bd8d41b80a85bc071f9c
Bierschneider Christian <christian.bierschneider@web.de>
2022-10-21 17:02:46 +0200

    Updated the bflyServerApp tests

--- webserver/bflyServerApp/test/test_rsa_decryption.py
+++ webserver/bflyServerApp/test/test_rsa_decryption.py
@@ -5,9 +5,23 @@ from bflyServerApp import RSADecryption
 class TestRSADecryption(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.rsadecryptor = RSADecryption()
+        self.rsadecryptor.generate_keys(keysize=2048)
 
     def tearDown(self) -> None:
+        pass
+
+    def test_get_private_key(self):
+        key = self.rsadecryptor.get_private_key()
+        print(key)
+
+    def test_get_public_key(self):
+        pass
+
+    def test_encrypt(self):
+        pass
+
+    def test_decrypt(self):
         pass
 
 
