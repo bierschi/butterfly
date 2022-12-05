cfae0b361bbd6790dca843d6907199b531c95c40
Bierschneider Christian <christian.bierschneider@web.de>
2022-12-05 18:37:07 +0100

    Furhter updates on createFilesystem script

--- scripts/README.md
+++ scripts/README.md
@@ -1,1 +1,8 @@
 # Scripts
+
+## Data files Sources
+
+<pre><code>
+https://file-examples.com/ 
+https://releases.ubuntu.com/22.04/
+</code></pre>
--- scripts/createFilesystem.py
+++ scripts/createFilesystem.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 import os
+import glob
 import shutil
 import requests
 import tarfile
+import argparse
 
 url = "http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-amd64.tar.gz"
+image_url = "https://releases.ubuntu.com/22.04/ubuntu-22.04.1-desktop-amd64.iso"
 target_folder = "../fs"
 target_file = "../fs/ubuntu-base-22.04-base-amd64.tar.gz"
 
 
 def create_fs_folder(folderpath):
-    """
+    """ creates the filesystem folder fs/
 
-    :return:
     """
     try:
         os.mkdir(folderpath)
@@ -21,9 +23,8 @@ def create_fs_folder(folderpath):
 
 
 def download_ubuntu_fs(filepath):
-    """
+    """ downloads the ubuntu fs tar.gz folder
 
-    :return:
     """
 
     resp = requests.get(url, stream=True)
@@ -34,15 +35,28 @@ def download_ubuntu_fs(filepath):
         print("Received invalid statuscode {} for downloading file {}".format(resp.status_code, filepath))
 
 
-def extract_tar_gz(folderpath, filepath):
+def download_ubuntu_image():
     """
 
-    :param fname:
     :return:
+    """
+    resp = requests.get(image_url, stream=True)
+    if resp.status_code == 200:
+        with open("data/ubuntu-22.04.1-desktop-amd64.iso", 'wb') as f:
+            f.write(resp.raw.read())
+    else:
+        print("Received invalid statuscode {} for downloading ubuntu image".format(resp.status_code))
+
+
+def extract_tar_gz(extractpath, filepath):
+    """ extracts the tar.gz/tar folder to folderpath
+
+    :param extractpath: folderpath to extract the tar.gz file
+    :param filepath: filepath to the tar.gz file
     """
 
     cwd = os.getcwd()
-    os.chdir(folderpath)
+    os.chdir(extractpath)
 
     if filepath.endswith("tar.gz"):
         tar = tarfile.open(filepath, "r:gz")
@@ -59,15 +73,46 @@ def extract_tar_gz(folderpath, filepath):
     os.chdir(cwd)
 
 
-def prepare_fs_with_data():
-    pass
+def prepare_fs_with_data(folderpath):
+    """ prepares the folderpath for data content
+
+    """
+
+    # create butterfly home directory
+    home_dir = folderpath + '/home/butterfly'
+    data_dir = home_dir + '/data'
+    big_dir = data_dir + '/big'
+    big_dir_sub = big_dir + '/subfolder'
+    small_dir = data_dir + '/small'
+    small_dir_sub = small_dir + '/subfolder'
+
+    create_fs_folder(home_dir)
+    create_fs_folder(data_dir)
+    create_fs_folder(big_dir)
+    create_fs_folder(big_dir_sub)
+    create_fs_folder(small_dir)
+    create_fs_folder(small_dir_sub)
+
+    for file in glob.glob('data/file*'):
+        shutil.copy(file, small_dir)
+        shutil.copy(file, small_dir_sub)
+
+    for file in glob.glob('data/zip*'):
+        shutil.copy(file, small_dir)
+    for file in glob.glob('data/*.exe'):
+        shutil.copy(file, small_dir)
+
+    for file in glob.glob('data/*'):
+        shutil.copy(file, big_dir)
+
+    for file in glob.glob('data/file*'):
+        shutil.copy(file, big_dir_sub)
 
 
 def remove_fs(folderpath):
-    """
+    """ removes the filesystem folder fs/
 
-    :param folderpath:
-    :return:
+    :param folderpath: path to the fs/ folder
     """
     try:
         shutil.rmtree(folderpath)
@@ -76,10 +121,27 @@ def remove_fs(folderpath):
 
 
 def main():
+
+    usage1 = "USAGE: \n\t./createFilesystem.py"
+
+    usage2 = "./createFileSystem.py "
+
+    description = "Creates the ubuntu filesystem and prepares the data content in home directory. \n\n {}\n        {}".format(usage1, usage2)
+
+    # parse arguments
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # arguments
+    #parser.add_argument('-f', '--file', type=str, help='')
+
+    # parse all arguments
+    args = parser.parse_args()
+
     print("Create example filesystem")
-    create_fs_folder(folderpath=target_folder)
-    download_ubuntu_fs(filepath=target_file)
-    extract_tar_gz(folderpath=target_folder, filepath=target_file)
+    #create_fs_folder(folderpath=target_folder)
+    #download_ubuntu_fs(filepath=target_file)
+    #extract_tar_gz(extractpath=target_folder, filepath=target_file)
+    #prepare_fs_with_data(folderpath=target_folder)
     #remove_fs(folderpath=target_folder)
 
 
