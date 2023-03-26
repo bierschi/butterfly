ca8e533589b0b98c6aee06a52d9fba96480a6390
Bierschneider Christian <christian.bierschneider@web.de>
2023-03-26 18:47:33 +0200

    Updated the httpRequestTest case

--- test/httpRequestTest.cpp
+++ test/httpRequestTest.cpp
@@ -11,27 +11,29 @@ class HTTPRequestTest : public ::testing::Test
 {
 
 protected:
-    std::string url = "www.google.de", formParamStr;
-    std::vector< std::pair<std::string, std::string> > formParams;
+    std::string _url = "www.google.de", _userAgent = "butterfly", _contentType = "application/x-www-form-urlencoded", _testRoute = "/testroute", _formParamStr;
+    butterfly::Method _method = butterfly::Method::POST;
+    butterfly::Protocol _protocol = butterfly::Protocol::HTTP1_1;
+    std::vector< std::pair<std::string, std::string> > _formParams;
     std::unique_ptr<butterfly::HTTPRequest> httpRequest;
 
     void SetUp() override
     {
         httpRequest = std::unique_ptr<butterfly::HTTPRequest>(new butterfly::HTTPRequest());
         // Set http request data
-        httpRequest->setURL(url);
-        httpRequest->setMethod(butterfly::Method::POST);
-        httpRequest->setProtocol(butterfly::Protocol::HTTP1_1);
-        httpRequest->setHTTPHeader("User-Agent", "butterfly");
-        httpRequest->setHTTPHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpRequest->setURL(_url);
+        httpRequest->setMethod(_method);
+        httpRequest->setProtocol(_protocol);
+        httpRequest->setHTTPHeader("User-Agent", _userAgent);
+        httpRequest->setHTTPHeader("Content-Type", _contentType);
 
-        formParams.emplace_back(butterfly::params::ENC_CPRIVATERSA_FILENAME, "abcdef123");
-        formParams.emplace_back(butterfly::params::RSA_EKIV_FILENAME, "abcdef123");
-        formParams.emplace_back("RSAKeySize", std::to_string(butterfly::params::RSA_KEYSIZE));
+        _formParams.emplace_back(butterfly::params::ENC_CPRIVATERSA_FILENAME, "abcdef123");
+        _formParams.emplace_back(butterfly::params::RSA_EKIV_FILENAME, "abcdef123");
+        _formParams.emplace_back("RSAKeySize", std::to_string(butterfly::params::RSA_KEYSIZE));
 
-        httpRequest->addFormParamVector(formParams);
-        formParamStr = httpRequest->getFormParam();
-        httpRequest->addBody(formParamStr);
+        httpRequest->addFormParamVector(_formParams);
+        _formParamStr = httpRequest->getFormParam();
+        httpRequest->addBody(_formParamStr);
         httpRequest->setHTTPHeader("Content-Length", std::to_string(httpRequest->getBody().length()));
 
         httpRequest->prepareOutgoing();
@@ -44,34 +46,45 @@ protected:
 };
 
 /**
- * Testcase for testing the getMethod
+ * Testcase for testing the getMethod method
  */
 TEST_F(HTTPRequestTest, getMethod)
 {
     butterfly::Method method = httpRequest->getMethod();
 
-    EXPECT_TRUE(method == butterfly::Method::POST);
+    EXPECT_TRUE(method == _method);
 }
 
 /**
- * Testcase for testing the getURL
+ * Testcase for testing the getURL method
  */
 TEST_F(HTTPRequestTest, getURL)
 {
     std::string url = httpRequest->getURL();
 
-    EXPECT_TRUE(url == url);
+    EXPECT_TRUE(url == _url);
 }
 
 /**
- * Testcase for testing the getUserAgent
+ * Testcase for testing the getUserAgent method
  */
 TEST_F(HTTPRequestTest, getUserAgent)
 {
-    httpRequest->setUserAgent("butterfly");
+    httpRequest->setUserAgent(_userAgent);
     std::string useragent = httpRequest->getUserAgent();
 
-    EXPECT_TRUE(useragent == "butterfly");
+    EXPECT_TRUE(useragent == _userAgent);
+}
+
+/**
+ * Testcase for testing the getRoute method
+ */
+TEST_F(HTTPRequestTest, getRoute)
+{
+    httpRequest->setRoute(_testRoute);
+    std::string route = httpRequest->getRoute();
+
+    EXPECT_TRUE(route == _testRoute);
 }
 
 /**
@@ -91,7 +104,7 @@ TEST_F(HTTPRequestTest, getHTTPData)
 {
     std::string httpdata = httpRequest->getHTTPData();
 
-    EXPECT_TRUE(!httpdata.empty());
+    EXPECT_TRUE( !httpdata.empty() );
 }
 
 /**
@@ -102,7 +115,7 @@ TEST_F(HTTPRequestTest, getProtocol)
 
     butterfly::Protocol prot = httpRequest->getProtocol();
 
-    EXPECT_TRUE(prot == butterfly::Protocol::HTTP1_1);
+    EXPECT_TRUE(prot == _protocol);
 }
 
 /**
@@ -112,7 +125,7 @@ TEST_F(HTTPRequestTest, getBody)
 {
     std::string body = httpRequest->getBody();
 
-    EXPECT_TRUE(body == formParamStr);
+    EXPECT_TRUE(body == _formParamStr);
 }
 
 /**
@@ -121,8 +134,8 @@ TEST_F(HTTPRequestTest, getBody)
 TEST_F(HTTPRequestTest, getHTTPHeader)
 {
     std::string httpHeaderUserAgent = httpRequest->getHTTPHeader("User-Agent");
-    std::string httpHeaderContent = httpRequest->getHTTPHeader("Content-Type");
+    std::string httpHeaderContentType = httpRequest->getHTTPHeader("Content-Type");
 
-    EXPECT_TRUE(httpHeaderUserAgent == "butterfly");
-    EXPECT_TRUE(httpHeaderContent == "application/x-www-form-urlencoded");
+    EXPECT_TRUE(httpHeaderUserAgent == _userAgent);
+    EXPECT_TRUE(httpHeaderContentType == _contentType);
 }
