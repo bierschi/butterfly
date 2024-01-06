2a3bdca1f67770391a36739f86d64c909a55fc7e
Bierschneider Christian <christian.bierschneider@web.de>
2024-01-06 13:18:19 +0100

    Added new class CryptoProcess

---
+++ include_linux/crypto/cryptoProcess.h
@@ -0,0 +1,62 @@
+
+#ifndef BUTTERFLY_CRYPTOPROCESS_H
+#define BUTTERFLY_CRYPTOPROCESS_H
+
+#include <iostream>
+#include <vector>
+#include <sys/wait.h>
+
+#include "logger.h"
+
+namespace butterfly
+{
+
+namespace hybrid
+{
+
+/**
+* Class CryptoProcess to create dedicated processes for the Encryption and Decryption phase
+*/
+class CryptoProcess
+{
+
+private:
+    std::vector<pid_t> _processIds;
+
+    /**
+     * pure virtual method for the sub classes to implement
+     *
+     * @param filepath: path for the file
+     */
+    virtual void handleLargeFilesWithAES(const std::string& filepath) = 0;
+
+public:
+
+    /**
+     * Constructor CryptoProcess
+     */
+    CryptoProcess();
+
+    /**
+     * Virtual Destructor CryptoProcess
+     */
+    virtual ~CryptoProcess();
+
+    /**
+     * Creates the process with filepath as parameter
+     *
+     * @param filepath: path to the file
+     */
+    void create(const std::string &filepath);
+
+    /**
+     * Waits for all previous created processes
+     */
+    void waitAll();
+};
+
+} // namespace hybrid
+
+} // namespace butterfly
+
+#endif //BUTTERFLY_CRYPTOPROCESS_H
